b3b079be09a8f3127d9875df6eba7a6cf6c12459
Vishal Kumar <hello@vishalk.com>
2024-01-27 18:56:39 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { fontDisplay, fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
@@ -26,6 +27,7 @@ export default function RootLayout({
         </div>
         <main className="container py-20">{children}</main>
         <TailwindIndicator />
+        <Analytics />
       </body>
     </html>
   );
